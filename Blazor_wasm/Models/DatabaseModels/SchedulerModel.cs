e351ef7a8c270f28ffdc3513a32022fd6c7eafa5
weichen LI <wang8119@gmail.com>
2025-05-26 18:35:57 +0800

    Modify Schedule & Schedule Model

--- Blazor_wasm/Models/DatabaseModels/SchedulerModel.cs
+++ Blazor_wasm/Models/DatabaseModels/SchedulerModel.cs
@@ -17,10 +17,10 @@ namespace Blazor_wasm.Models.DatabaseModels
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public bool? IsAllDay { get; set; }
-        public string RecurrenceRule { get; set; } = "";
-        public string StartTimezone { get; set; } = "";
-        public string EndTimezone { get; set; } = "";
         public bool SpecifiedDay { get; set; }
         public int IntervalNumber { get; set; } = 1;
+        public string RecurrenceRule { get; set; } = "";
+        public string StartTimezone { get; set; } = "";
+        public string EndTimezone { get; set; } = "";      
     }
 }
